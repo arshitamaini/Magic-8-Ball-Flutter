e0750b5ce5dedbda4d7b8d79c97e805f9dd25145
Arshita Maini <arshita.maini@gmail.com>
2021-09-24 20:27:53 +0530

    Assignment#2

--- lib/main.dart
+++ lib/main.dart
@@ -15,7 +15,7 @@ class MagicBallPage extends StatelessWidget {
         backgroundColor: Colors.blueGrey,
         appBar: AppBar(
           backgroundColor: Colors.blueAccent,
-          title: Text('Ask Me Anything'),
+          title: Center(child: Text('Ask Me Anything')),
         ));
   }
 }
@@ -31,19 +31,25 @@ class _MagucBallState extends State<MagucBall> {
   int ballnumber = 1;
   void randomball() {
     setState(() {
-      ballnumber = Random().nextInt(4) + 1;
+      ballnumber = Random().nextInt(5) + 1;
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    return Row(children: [
-      TextButton(
+    return Container(
+        child: Center(
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: TextButton(
+          child: Image.asset('images/ball$ballnumber.png'),
           onPressed: () {
-            randomball();
-            print(ballnumber);
+            setState(() {
+              randomball();
+            });
           },
-          child: Image.asset('images/ball$ballnumber.png'))
-    ]);
+        ),
+      ),
+    ));
   }
 }
